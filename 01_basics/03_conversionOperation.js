391d9d8fb7bc19f54ce6ee591bd15a700b782395
Dheeraj <126350932+dheeraj1952k1@users.noreply.github.com>
2024-03-02 05:09:48 +0000

    conversion of dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,7 +7,7 @@ console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 // "33" => 33
-// "33abc" => NaN
+// "33abc" => NaN  // not a number
 //true=> 1;
 
 let isLoggedIn = 1
@@ -17,6 +17,7 @@ console.log(booleanIsLoggedIn);
 
 // 1=> true , 0 => false
 //"" false
+//"hitesh" => true
 
 let number = 5
 let stringNumber = String(number)
